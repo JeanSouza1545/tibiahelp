d1b7a1638e5b1dc91e90b064bc4dddc298962010
Jean Gomes de Souza <jgsouza@coamo.com.br>
2026-03-09 15:13:01 -0300

    Correções de layout a amplialção no usa de styleToken

--- src/Components/generalFooter/index.tsx
+++ src/Components/generalFooter/index.tsx
@@ -3,14 +3,16 @@ import style from './footer.module.css'
 
 const GeneralFooter = () => {
     return(
-        <footer className={style.footerContainer}>
-            <p className={style.footerText}>
-                &copy; 2026 TibiaHelp - Sua fonte de ajuda para Tibia.
-            </p>
-            <p className={style.smallFooterText}>
-                Este site não é afiliado com a CipSoft.
-            </p>
-        </footer>
+        // <div>
+            <footer className={style.footerContainer}>
+                <p className={style.footerText}>
+                    &copy; 2026 TibiaHelp - Sua fonte de ajuda para Tibia.
+                </p>
+                <p className={style.smallFooterText}>
+                    Este site não é afiliado com a CipSoft.
+                </p>
+            </footer>
+        // </div>
     )
 }
 
